5d3aea532bb8a14cdcb852cdaaa8133c6e4f2f90
asadbek0512 <khusanovasadbek777@gmail.com>
2024-12-11 14:30:12 +0900

    feat: H2 - TASK

--- src/train.ts
+++ src/train.ts
@@ -15,4 +15,19 @@ function getPositive(arrey: number[]) {
 
 console.log("Musbat sonlar: ", getPositive([1, -4, -2, 0]));
 
+// H2-TASK: 
+// Shunday function tuzing, unga string argument pass bolsin. 
+// Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
 
+function getDigits(son: string) {
+    let qiymat: string = " ";
+    for (let i = 0; i < son.length; i++) {
+        if (son[i] >= "0" && son[i] <= "9") {
+            qiymat += son[i];
+        }
+    }
+    return qiymat;
+}
+
+console.log("result: ", getDigits("m14i1t"));
