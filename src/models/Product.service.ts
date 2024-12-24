f22addba6ba96862b9bccbfac6805994cd3b08a1
asadbek0512 <khusanovasadbek777@gmail.com>
2024-12-25 08:37:25 +0900

    fix: configure product Schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,3 +1,11 @@
-class ProductService { }
+import ProductModel from "../schema/Product.model"; //? ProductModel qnaday paydo bo'lib qolyapti
+
+class ProductService {
+    private readonly productModel;
+
+    constructor() {
+        this.productModel = ProductModel;
+    }
+}
 
 export default ProductService;
