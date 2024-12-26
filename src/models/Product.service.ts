20a6cba12d529517053b3b70a499e8cf593b924c
asadbek0512 <khusanovasadbek777@gmail.com>
2024-12-26 11:26:12 +0900

    fix: modify updateChosenProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,6 +1,7 @@
+import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Product, ProductInput } from "../libs/types/product";
-import ProductModel from "../schema/Product.model"; //? ProductModel qnaday paydo bo'lib qolyapti
+import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
+import ProductModel from "../schema/Product.model";
 
 class ProductService {
     private readonly productModel;
@@ -15,12 +16,32 @@ class ProductService {
 
     public async createNewProduct(input: ProductInput): Promise<Product> {
         try {
-            return await this.productModel.create(input);
+            return await this.productModel.create(input); //?? buyerda nega exec() qoyilmagan  veribelga tenglamaganimiz sabablimi?
         } catch (err) {
             console.error("Error, model:creatNewProduct:");
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
     }
+
+    public async updateChosenProduct(
+        id: string,
+        input: ProductUpdateInput
+    ): Promise<Product> {
+        //string => objectid
+        id = shapeIntoMongooseObjectId(id);
+        const result = await this.productModel
+            .findByIdAndUpdate({ _id: id }, input, { new: true })
+            .exec();
+
+        if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+        console.log("result: ", result);
+        return result;
+    }
+
 }
+
+
+
 
 export default ProductService;
