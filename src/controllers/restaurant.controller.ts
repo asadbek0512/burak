3ad81edb3a35888a99fbd97321878522cb26c5fe
asadbek0512 <khusanovasadbek777@gmail.com>
2025-01-05 08:36:38 +0900

    feat: frontend chosen user update logic develop

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -107,7 +107,6 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
     try {
         console.log('getUser');
         const result = await memberService.getUsers()
-        console.log("result:", result);
 
         res.render("users", { users: result });
     } catch (err) {
